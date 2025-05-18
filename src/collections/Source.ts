65019a64db07e61cd42768df5237636f1b245dbf
Joshua Schmidt <joshuaschmidt@Joshuas-MacBook-Air.local>
2025-05-18 14:52:23 +0300

    fix/corepack

---
+++ src/collections/Source.ts
@@ -0,0 +1,38 @@
+import { CollectionConfig } from 'payload'
+
+export const Source: CollectionConfig = {
+  slug: 'source',
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'type',
+      type: 'select',
+      options: ['rss'],
+      required: true,
+    },
+    {
+      name: 'url',
+      type: 'text',
+    },
+    {
+      name: 'isActive',
+      type: 'checkbox',
+      defaultValue: true,
+    },
+    {
+      name: 'categories',
+      type: 'text',
+      hasMany: true,
+    },
+    {
+      name: 'owner',
+      type: 'relationship',
+      relationTo: 'users',
+      hasMany: false,
+    },
+  ],
+}
